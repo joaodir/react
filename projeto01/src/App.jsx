c6243841c3f349ca0f2c53565b7c456067af33b5
joao <joaodirceuconstantinocarvalho@gmail.com>
2026-01-06 21:12:54 -0300

    v1.0 - feat: Secao adicionada

--- projeto01/src/App.jsx
+++ projeto01/src/App.jsx
@@ -1,10 +1,12 @@
 import Cabecalho from "./Components/Cabecalho"
 import Rodape from "./Components/Rodape"
+import Secao from "./Components/Secao";
 function App() {
 
   return (
     <>
       <Cabecalho />
+      <Secao />
       <Rodape />
     </>
   )
---
+++ projeto01/src/Components/Secao.jsx
@@ -0,0 +1,10 @@
+const Secao = () => {
+  return (
+    <>
+      <h2>Seção de Conteúdo</h2>
+      <p>Este é um exemplo de seção em um componente React.</p>
+    </>
+  );
+};
+
+export default Secao;
